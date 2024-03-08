55cb2c89406c0947a5a93bb7e921ead33e1bee3b
Julia Vorotchenko <juliavorotchenko@gmail.com>
2024-03-08 19:06:36 +0200

    Add get birthdays

--- Bot/AddressBook/address_book.py
+++ Bot/AddressBook/address_book.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from datetime import datetime, timedelta
 
 
 class AddressBook(UserDict):
@@ -10,6 +11,19 @@ class AddressBook(UserDict):
 
     def delete(self, name):
         del self.data[name]
+
+    def get_birthdays_per_week(self):
+        today = datetime.now()
+        next_week = today + timedelta(days=7)
+
+        birthdays_next_week = []
+        for record in self.values():
+            if record.birthday:
+                contact_birthday = datetime.strptime(str(record.birthday), "%d.%m.%Y")
+                if today <= contact_birthday < next_week:
+                    birthdays_next_week.append(str(record.name))
+
+        return birthdays_next_week
 
     def __str__(self):
         return '\n'.join(map(str, self.values()))
